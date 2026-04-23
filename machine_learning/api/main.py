223248cc3deabcdba33cf6b3a3ea8ea0eba70d65
Mikolaj <mikolajwiedzmin@gmail.com>
2026-04-23 12:57:59 +0200

    feat: add api

---
+++ machine_learning/api/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import joblib, numpy as np, os
+
+app = FastAPI(title="Chess Asistance Models API")
+
+MODEL_PATH  = "../trainer/models/model.pkl"
+SCALER_PATH = "../trainer/models/scaler.pkl"
+
+if not os.path.exists(MODEL_PATH):
+    raise RuntimeError("model.pkl not found in /models/")
+
+model  = joblib.load(MODEL_PATH)
+scaler = joblib.load(SCALER_PATH)
+
+class ChanceWinrateFeatures(BaseModel):
+    minutes_slept: int
+    minutes_awake:  int
+    temperature: int
+    co2:  int
+
+@app.post("/predict")
+def predict(data: ChanceWinrateFeatures):
+    X = np.array([[
+        data.minutes_slept, data.minutes_awake,
+        data.temperature, data.co2
+    ]])
+    X_scaled = scaler.transform(X)
+    proba = model.predict_proba(X_scaled)[0]
+    return {
+        "Chance": 100
+    }
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
