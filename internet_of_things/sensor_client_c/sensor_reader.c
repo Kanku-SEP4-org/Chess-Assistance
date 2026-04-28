e08449a35689cd73709fa8fe1f45ac8954baeebc
NataliaMuchova <nataliiamuchova@gmail.com>
2026-04-28 15:10:57 +0200

    feat: connection between rabbitmq and arduino established

--- internet_of_things/sensor_client_c/sensor_reader.c
+++ internet_of_things/sensor_client_c/sensor_reader.c
@@ -1,68 +1,92 @@
 #include "sensor_reader.h"
 #include <stdio.h>
 #include <string.h>
-// #include <fcntl.h>
-// #include <unistd.h>
-// #include <termios.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
 
 #define SERIAL_PORT "/dev/ttyACM0"
 // #define SERIAL_PORT "/dev/ttyUSB0"
 
-/* settings for Linux to talk to Arduino
-
-
+// settings for Linux to talk to Arduino
 
 int setup_serial(int serial)
 {
     struct termios tty;
     memset(&tty, 0, sizeof(tty));
 
-    if (tcgetattr(serial, &tty) != 0)
-    {
-        return 0;
-    }
+    tcgetattr(serial, &tty);
 
     cfsetospeed(&tty, B115200);
     cfsetispeed(&tty, B115200);
 
-    tty.c_cflag = CS8 | CLOCAL | CREAD;
-    tty.c_iflag = 0;
-    tty.c_oflag = 0;
-    tty.c_lflag = 0;
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;      
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;          
+    tty.c_cflag &= ~CRTSCTS;       
+    tty.c_cflag |= CREAD | CLOCAL; 
+
+    tty.c_lflag &= ~ICANON;        
+    tty.c_lflag &= ~ECHO;
+    tty.c_lflag &= ~ECHOE;
+    tty.c_lflag &= ~ISIG;
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); 
+    tty.c_iflag &= ~(ICRNL | INLCR);        
+
+    tty.c_oflag &= ~OPOST;         
 
     tty.c_cc[VMIN] = 0;
-    tty.c_cc[VTIME] = 20;
+    tty.c_cc[VTIME] = 20;          
 
-    if (tcsetattr(serial, TCSANOW, &tty) != 0)
-    {
-        return 0;
-    }
+    tcsetattr(serial, TCSANOW, &tty);
 
     return 1;
 }
-*/
+
 
 int read_temperature(float *temperature)
 {
-    // int serial = open(SERIAL_PORT, O_RDWR | O_NOCTTY);
-
-    // setup_serial(serial);
-
-    FILE *sensorOutput = popen("../mock_sensor_c/mock-sensor", "r");
+    int serial = open(SERIAL_PORT, O_RDWR | O_NOCTTY);
+    if (serial == -1)
+    {
+        printf("open_port: Unable to open");
+        return -1;
+    }
+    
+    setup_serial(serial);
 
     char buffer[100] = {0};
 
-    fgets(buffer, sizeof(buffer), sensorOutput);
+    sleep(2); // arduino may reset when port opens
+    tcflush(serial, TCIOFLUSH); // clear old data
 
-    pclose(sensorOutput);
+    write(serial, "1", 1); // asks Arduino for temperature, send 1, 1 character
 
-    // write(serial, "1\n", 2); // asks Arduino for temperature
+    usleep(500000); // wait for arduino to answer
 
-    // read(serial, buffer, sizeof(buffer) - 1);
+    read(serial, buffer, sizeof(buffer) - 1);
 
-    // close(serial);
+    close(serial);
 
     sscanf(buffer, "TEMP:%f", temperature);
 
     return 1;
 }
+
+
+
+// in administrator powershell
+
+// usbipd list - This should show arduino connected 'SHARED'
+
+// usbipd attach --wsl --busid 1-2(or whatever it said before when connecting to the port) 
+
+
+// in wsl
+
+// ls /dev/ttyUSB* /dev/ttyACM* 2>/dev/null - This checks which Arduino serial port exists in WSL.
+
+// sudo chmod a+rw /dev/ttyACM0 - This gives permission to read and write to the Arduino port.
+
