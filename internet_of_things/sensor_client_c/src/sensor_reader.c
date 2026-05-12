dcc28f81e047bcd77dcb9ce746679b41f7bef59a
Ana-Maria Patriche <133980298+capkauna@users.noreply.github.com>
2026-05-12 14:07:27 +0200

    fix: resolved merge conflicts tx to file restructured

--- internet_of_things/sensor_client_c/src/sensor_reader.c
+++ internet_of_things/sensor_client_c/src/sensor_reader.c
@@ -23,32 +23,34 @@ int setup_serial(int serial)
     // This code ONLY exists for Linux use (arduino connected)
     struct termios tty;
     memset(&tty, 0, sizeof(tty));
-    if (tcgetattr(serial, &tty) != 0) return -1;
+
+    tcgetattr(serial, &tty);
 
     cfsetospeed(&tty, B115200);
     cfsetispeed(&tty, B115200);
 
     tty.c_cflag &= ~PARENB;
-    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CSTOPB;      
     tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;
-    tty.c_cflag &= ~CRTSCTS;
-    tty.c_cflag |= CREAD | CLOCAL;
+    tty.c_cflag |= CS8;          
+    tty.c_cflag &= ~CRTSCTS;       
+    tty.c_cflag |= CREAD | CLOCAL; 
 
-    tty.c_lflag &= ~ICANON;
+    tty.c_lflag &= ~ICANON;        
     tty.c_lflag &= ~ECHO;
     tty.c_lflag &= ~ECHOE;
     tty.c_lflag &= ~ISIG;
 
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-    tty.c_iflag &= ~(ICRNL | INLCR);
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); 
+    tty.c_iflag &= ~(ICRNL | INLCR);        
 
-    tty.c_oflag &= ~OPOST;
+    tty.c_oflag &= ~OPOST;         
 
     tty.c_cc[VMIN] = 0;
-    tty.c_cc[VTIME] = 20;
+    tty.c_cc[VTIME] = 20;          
 
     tcsetattr(serial, TCSANOW, &tty);
+
     return 1;
 #else
     // Windows dummy return
@@ -62,28 +64,32 @@ int read_temperature(float *temperature)
 #if !defined(_WIN32) && !defined(UNIT_TESTING)
     // --- REAL LINUX LOGIC ---
     int serial = open(SERIAL_PORT, O_RDWR | O_NOCTTY);
-    if (serial == -1) {
-        printf("open_port: Unable to open %s\n", SERIAL_PORT);
+    if (serial == -1)
+    {
+        printf("open_port: Unable to open");
         return -1;
     }
-
+    
     setup_serial(serial);
+
     char buffer[100] = {0};
 
-    sleep(2);
-    tcflush(serial, TCIOFLUSH);
+    sleep(2); // arduino may reset when port opens
+    tcflush(serial, TCIOFLUSH); // clear old data
 
     write(serial, "1\n", 2);
+
     usleep(500000);
+
     read(serial, buffer, sizeof(buffer) - 1);
+
     close(serial);
 
     char *temp_pos = strstr(buffer, "TEMP:");
-    if (temp_pos) {
+    if (temp_pos)
         sscanf(temp_pos, "TEMP:%f", temperature);
-        return 1;
-    }
-    return -1;
+
+    return 1;
 #else
     // --- WINDOWS CLOUD MOCK ---
     // This allows testing the Message Builder/RabbitMQ without an Arduino
@@ -91,3 +97,56 @@ int read_temperature(float *temperature)
     return 1;
 #endif
 }
+
+int read_light(short *light)
+{
+#if !defined(_WIN32) && !defined(UNIT_TESTING)
+    // --- REAL LINUX LOGIC ---
+    int serial = open(SERIAL_PORT, O_RDWR | O_NOCTTY);
+    if (serial == -1)
+    {
+        printf("open_port: Unable to open");
+        return -1;
+    }
+    
+    setup_serial(serial);
+
+    char buffer[100] = {0};
+
+    sleep(2);
+    tcflush(serial, TCIOFLUSH);
+
+    write(serial, "5\n", 2);
+
+    usleep(500000);
+
+    read(serial, buffer, sizeof(buffer) - 1);
+
+    close(serial);
+
+    char *light_pos = strstr(buffer, "LIG:");
+    if (light_pos)
+        sscanf(light_pos, "LIG:%hd", light);
+
+    return 1;
+#else
+    // --- WINDOWS CLOUD MOCK ---
+    // This allows testing the Message Builder/RabbitMQ without an Arduino
+    *light = 500;
+    return 1;
+#endif
+}
+
+// in administrator powershell
+
+// usbipd list - This should show arduino connected 'SHARED'
+
+// usbipd attach --wsl --busid 1-2(or whatever it said before when connecting to the port) 
+
+
+// in wsl
+
+// ls /dev/ttyUSB* /dev/ttyACM* 2>/dev/null - This checks which Arduino serial port exists in WSL.
+
+// sudo chmod a+rw /dev/ttyACM0 - This gives permission to read and write to the Arduino port.
+
