091f87855233aaf1b920499b56e96be8cea6a6c7
NataliaMuchova <nataliiamuchova@gmail.com>
2026-05-13 13:49:41 +0200

    fix: changed names for entity classes. feat: AppDbContext class implemented.

--- services/iot-service/Grpc-Server/Entities/CO2Sensor.cs
+++ services/iot-service/Grpc-Server/Entities/CO2Sensor.cs
@@ -2,13 +2,13 @@ using System.Text.RegularExpressions;
 
 namespace IoTGrpcServer;
 
-public class WaterSensorEntity
+public class CO2Sensor
 {
     public int Id {get; set;}
 
     public DateTime TimeStamp {get; set;}
 
-    public int Ml {get; set;}
+    public int Ppm {get; set;}
 
     public Room Room {get; set;}
     public int RoomId {get; set;}
