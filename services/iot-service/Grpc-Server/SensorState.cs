2513e0d99e02bde3ff7b36b55b48db44e858c7d0
pimbp <pimbp@outlook.com>
2026-05-15 00:06:26 +0200

    fix: rebase fix

--- services/iot-service/Grpc-Server/SensorState.cs
+++ services/iot-service/Grpc-Server/SensorState.cs
@@ -6,5 +6,8 @@ public class SensorState
     public float Value { get; set; }
     public long Timestamp { get; set; }
     public string Type { get; set; } = string.Empty;
+<<<<<<< HEAD
     public bool Recording { get; set; } = false;
+=======
+>>>>>>> 6090698 (fix: cherry-pick unmerged msg queue implementation)
 }
