ebe01f7451e77eae893ccf685605eacbde413b43
qVanished <285800@via.dk>
2026-05-19 22:06:51 +0200

    Tests done for water pump and arduino code

--- internet_of_things/sensor_client_c/tests/test_message_builder.c
+++ internet_of_things/sensor_client_c/tests/test_message_builder.c
@@ -9,6 +9,9 @@ static float mock_temp_value;
 static int mock_water_result;
 static int mock_water_value;
 
+static int mock_pump_result;
+static int mock_pump_success;
+
 // If the temp_value is true, it returns success and writes the test value. Else, it will simulate a failed sensor read
 // int read_temperature(float *temperature)
 // {
@@ -31,6 +34,15 @@ static int mock_water_value;
 //     return 0;
 // }
 
+int read_pump_status(int *success)
+{
+    if (mock_pump_result)
+    {
+        *success = mock_pump_success;
+        return 1;
+    }
+    return 0;
+}
 // It runs before each test. It resets each value so ever test starts from a clean known state
 void setUp(void)
 {
@@ -39,6 +51,8 @@ void setUp(void)
 
     // mock_water_result = 1;
     // mock_water_value = 512;
+    mock_pump_result = 1;
+    mock_pump_success = 1;
 }
 
 // It runs AFTER each test and it does not require any cleanup. The function is required by unity.
@@ -97,7 +111,49 @@ void test_create_water_message_failure(void)
     TEST_ASSERT_NOT_NULL(strstr(buffer, "\"type\":\"water\""));
     TEST_ASSERT_NOT_NULL(strstr(buffer, "\"value\": null"));
 }
+/* Tests pump success JSON */
+void test_create_pump_response_message_done(void)
+{
+    char buffer[512] = {0};
+    mock_pump_result = 1;
+    mock_pump_success = 1;
 
+    create_pump_response_message(buffer);
+
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"arduinoId\":1"));
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"type\":\"pump\""));
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"status\":\"done\""));
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"timestamp\":"));
+}
+
+/* Tests pump fail JSON */
+void test_create_pump_response_message_fail(void)
+{
+    char buffer[512] = {0};
+    mock_pump_result = 1;
+    mock_pump_success = 0;
+
+    create_pump_response_message(buffer);
+
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"arduinoId\":1"));
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"type\":\"pump\""));
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"status\":\"fail\""));
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"timestamp\":"));
+}
+
+/* Tests pump error JSON */
+void test_create_pump_response_message_error(void)
+{
+    char buffer[512] = {0};
+    mock_pump_result = 0;
+
+    create_pump_response_message(buffer);
+
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"arduinoId\":1"));
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"type\":\"pump\""));
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"status\":\"error\""));
+    TEST_ASSERT_NOT_NULL(strstr(buffer, "\"timestamp\":"));
+}
 //The main Unity test runner
 int main(void)
 {
@@ -107,6 +163,8 @@ int main(void)
     // RUN_TEST(test_create_temperature_message_failure);
     // RUN_TEST(test_create_water_message_success);
     // RUN_TEST(test_create_water_message_failure);
-
+    // RUN_TEST(test_create_pump_response_message_done);
+    // RUN_TEST(test_create_pump_response_message_fail);
+    // RUN_TEST(test_create_pump_response_message_error);
     return UNITY_END();
 }
